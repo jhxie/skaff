4f8a6ffc49d0f175e7f4933650e749b688414a90
Jiahui Xie <jiahui.xie@outlook.com>
2016-05-03 15:36:22 -0600

    Fix a missing '__email__' import bug in the init module

--- genmake/__init__.py
+++ genmake/__init__.py
@@ -3,6 +3,7 @@
 from genmake.genmake import genmake
 from genmake.genmake import genmake_version_get
 from genmake.genmake import __author__
+from genmake.genmake import __email__
 from genmake.genmake import __license__
 from genmake.genmake import __maintainer__
 from genmake.genmake import __version__
