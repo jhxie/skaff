a375615adf249fb2c49ec565e924e01684f4fc79
Jiahui Xie <jiahui.xie@outlook.com>
2016-05-30 21:29:56 -0600

    Change the theme to 'default' in 'doc/source/conf.py'

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 import genmake
 # -- General configuration ------------------------------------------------
@@ -128,7 +128,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
